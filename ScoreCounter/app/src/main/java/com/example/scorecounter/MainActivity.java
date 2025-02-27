17e71510637642a6b6e2627db2a2f3b9a5414d30
PrahladBathre <mbathre0@gmail.com>
2025-02-28 04:39:14 +0530

    New Android

---
+++ ScoreCounter/app/src/main/java/com/example/scorecounter/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.scorecounter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button plus = findViewById(R.id.plus);
+        Button minus = findViewById(R.id.minus);
+        TextView result = findViewById(R.id.result);
+
+//        recognize the button to click or listen
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                result.setText(String.valueOf(count));
+            }
+
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                result.setText(String.valueOf(count));
+            }
+        });
+//        based on button update the value
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+    }
+}
+
+//when you create an application you get one activity by default
+//each activity will have one java file and one xml file
+//xml file will have the design
+//java file displays the sml file to the user and also provide logical supports to the design
