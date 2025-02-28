48a4614ca8b275b251efc83a98ad026ef362a939
PrahladBathre <mbathre0@gmail.com>
2025-02-28 18:50:17 +0530

    New apps

--- ExplicitIntent/app/src/main/java/com/example/explicitintent/MainActivity.java
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.activitylifecycle;
+package com.example.explicitintent;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,53 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+ EditText mEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mEditText = findViewById(R.id.friend_name);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("MAIN3", "OnStart()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("MAIN3", "OnResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i("MAIN3", "OnPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("MAIN3", "OnStop()");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i("MAIN3", "OnRestart()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("MAIN3", "OnDestroy()");
+    public void greetFriend(View view) {
+//        Logic to create once greet
+//        Open need to send data and display
+        String name = mEditText.getText().toString();
+//        create explicit intent
+        Intent i = new Intent(this,greetActivity.class);
+//        send intent request
+        i.putExtra("KEY",name);
+        startActivity(i);
     }
 }
---
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/greetActivity.java
@@ -0,0 +1,32 @@
+package com.example.explicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class greetActivity extends AppCompatActivity {
+    TextView greet;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_greet);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    greet = findViewById(R.id.greet_tv);
+//    get the data from intent object
+        Intent i = getIntent();
+        String name = i.getStringExtra("KEY");
+        greet.setText(String.format("Happy Birthday \n%s", name));
+    }
+}
